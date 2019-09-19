a93c49105f57009df5f9e008ade1f2e3345c6572
Stephen Bird <sbird@fluvialsystemsresearch.com>
2019-09-19 13:12:17 -0700

    Update

--- scripts/LW_input_Ims.R
+++ scripts/LW_input_Ims.R
@@ -30,15 +30,15 @@ VRI <-  st_intersection(VRI, basin_area) # crop VRI data by study area
 # Calculate LW inputted from mass movements-------------------------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Select only the slides adjacent to the study reach---------------------------------------------------------------
-slides <- subset(slides, Name == 2| Name ==3| Name ==4 | Name ==16)  
+slides <- subset(slides, Name == "B1"| Name =="B2"| Name =="B3" | Name =="DC")  
 
 # Attribute volume/ha data to slides based on nearby polygons-----------------------------------------------------
-Vs_16 <- mean(c(77.9, 86.176, 173.254, 47.983, 206.909))  
-Vs_2 <- 80.149 # slides 2-4 were present in 1971, so perhaps input from them is actually negligable?
-Vs_3 <- mean(c(80.149, 131.702))
-Vs_4 <- 80.149
-Vs_slides <- c(Vs_2, Vs_3, Vs_4, Vs_16)
-slides$Vs <- Vs_slides # add volume/ha estimates to dataframe
+Ds_DC <- mean(c(77.902, 64.189, 86.176, 257.785, 296.395))  
+Ds_B1 <- 80.149 # slides 2-4 were present in 1971, so perhaps input from them is actually negligable?
+Ds_B2 <- mean(c(80.149, 131.702))
+Ds_B3 <- 80.149
+Ds_slides <- c(Ds_B1, Ds_B2, Ds_B3, Ds_DC)
+slides$Ds <- Ds_slides # add volume/ha estimates to dataframe
 
 # Calculate area of each slide------------------------------------------------------------------------------------
 slides <- slides %>%  # in m2
@@ -51,8 +51,10 @@ plot(slides$geometry, add = TRUE, col = 'red')
 plot(streams$geometry, add = TRUE, col = 'darkblue')
 
 # Calculate I_ms = A_sf(V_sd + V_CWD) -----------------------------------------------------------------------------
-V_cwd = 5 # DUMMY VALUE --> an appropriate estimate may come from running the tipsy model for the area 
+V_cwd = 5 # DUMMY VALUE --> an appropriate estimate may come from running the tipsy model for the area. 
 
-slides$I_ms <- slides$Area*(slides$Vs + V_cwd) 
+slides$I_ms <- slides$Area*(slides$Ds + V_cwd) 
 slides.df <- as.data.frame(slides) 
 I_ms <- aggregate(slides.df['I_ms'], by=slides.df['Year'], sum) # sum input from mass movements by year
+
+slides$Vs = slides$Ds*slides$Area # estimate for pre-landslide volume of trees 
